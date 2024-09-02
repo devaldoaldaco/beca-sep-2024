47398d71781445a5fe3e93a117d7c46d80da68b1
Kevin Díaz Díaz <kevin.diazdiazian@gmail.com>
2024-09-02 08:03:45 -0600

    Create index.js

---
+++ tasks/kevin/clase05/ejercicio-11/index.js
@@ -0,0 +1,24 @@
+document.getElementById("returnHome").addEventListener("click", function(){
+    document.getElementById("home").scrollIntoView({ behavior: 'smooth'});
+});
+
+
+document.getElementById("seccionUno").addEventListener("click", function(){
+    document.getElementById("cupcakeSection").scrollIntoView({ behavior: 'smooth'});
+});
+
+document.getElementById("seccionDos").addEventListener("click", function(){
+    document.getElementById("hamburgerSection").scrollIntoView({ behavior: 'smooth'});
+});
+
+document.getElementById("seccionTres").addEventListener("click", function(){
+    document.getElementById("pasteSection").scrollIntoView({ behavior: 'smooth'});
+});
+
+document.getElementById("seccionCuatro").addEventListener("click", function(){
+    document.getElementById("menuSection").scrollIntoView({ behavior: 'smooth'});
+});
+
+document.getElementById("seccionCinco").addEventListener("click", function(){
+    document.getElementById("downloadSection").scrollIntoView({ behavior: 'smooth'});
+});
