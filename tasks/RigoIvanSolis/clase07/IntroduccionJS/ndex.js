b0f4c2724050f880f18d4b31d8eecef43055dc6e
Rigo1818 <165071145+Rigo1818@users.noreply.github.com>
2024-09-04 12:00:51 -0600

    Add files via upload

---
+++ tasks/RigoIvanSolis/clase07/IntroduccionJS/ndex.js
@@ -0,0 +1,23 @@
+console.log('Hola Rigo');
+
+function hola (){
+
+}
+
+hola();
+
+let contador = 0;
+
+for (let i = 0; i < 10; i++) {
+    contador += i;
+}
+
+console.log( 2 + 4 );
+
+
+// Cual es el resultado de este codigo
+[0,1,2,3].filter((item) => {
+    return item*2;
+}).filter()((item)=> {
+    return item > 2
+}).sort().join('');
