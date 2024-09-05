d19338026a8cae4b97e5bf9c87cfa28fe74319b5
kevindiaz1201 <kevin.diazdiazian@gmail.com>
2024-09-05 16:27:08 -0600

    Presentación

---
+++ tasks/kevinDiazDiaz/clase09/presentacion/index.js
@@ -0,0 +1,16 @@
+const KEVIN ={
+    nombre: 'Kevin',
+    edad: 22,
+    presentacion: function(){
+        console.log('Hola soy KEVIN');
+    },
+    tengoCarro: false,
+    comidaFavorita: 'enchiladas suizas',
+    familia: {
+        padre: 'Juan Manuel',
+        madre: 'Dulce Evelin',
+        hermano: 'Ian Kaleb'
+    }
+};
+
+KEVIN.presentacion();
