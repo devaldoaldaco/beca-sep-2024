11ed20385743298a79ff9932e1b0701bf5fb5978
Axelhz02 <axel.hernandez@ids.com.mx>
2024-09-12 13:10:31 -0600

    Add task

---
+++ tasks/axel/clase08/30Ejercicios/Ejercicio1.js
@@ -0,0 +1,15 @@
+let password = '1234';
+function contrasenaValida(){
+    if(password === '2Fj(jjbFsuj'){
+        console.log('La contraseña es valida');
+
+    }
+    else if (password === 'eoZiugBf&g9'){
+        console.log('La contrseña es valida');
+
+    }
+    else{
+        console.log('Contrseña incorrecta');
+    }
+}
+contrasenaValida();
