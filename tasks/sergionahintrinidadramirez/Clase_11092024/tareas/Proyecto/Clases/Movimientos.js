155240ca2dc013500ca496bc813c98cda6f2396f
Sergio Trinidad <sergio.trinidad@ids.com.mx>
2024-09-14 11:29:06 -0600

    feat: Initial push of full banking application with classes and web components

---
+++ tasks/sergionahintrinidadramirez/Clase_11092024/tareas/Proyecto/Clases/Movimientos.js
@@ -0,0 +1,7 @@
+export class Movimientos{
+    constructor(tipoPago,tipoMovimiento,importe){
+        this.tipoPago = tipoPago;
+        this.tipoMovimiento = tipoMovimiento;
+        this.importe = importe;
+    }
+}
---
+++ tasks/sergionahintrinidadramirez/Clase_11092024/tareas/Proyecto/index.js
@@ -0,0 +1,7 @@
+import { Login } from "./Componente/login.js";
+import { Dashboard } from "./Componente/dashboard.js";
+import { Details } from "./Componente/details.js";
+
+window.customElements.define("bbva-login", Login);
+window.customElements.define("bbva-dashboard", Dashboard);
+window.customElements.define("bbva-details", Details);
