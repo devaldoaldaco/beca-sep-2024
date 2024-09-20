59356ee466e515bdc9c8b77b609a581ec0b65845
Sergio Trinidad <sergio.trinidad@ids.com.mx>
2024-09-20 12:00:58 -0600

    feat: Integrated Random User API with fetch, displaying image and name in circular div

--- tasks/sergionahintrinidadramirez/Clase_19092024/tareas/index.js
+++ tasks/sergionahintrinidadramirez/Clase_19092024/tareas/index.js
@@ -1,37 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetchAndDisplayUsers();
-  });
-  
-  function fetchAndDisplayUsers() {
+});
+
+function fetchAndDisplayUsers() {
     fetch('https://randomuser.me/api/?results=16') //solicitar 16 registros aleatorios
-      .then(response => response.json())
-      .then(data => displayUsers(data.results))
-      .catch(error => console.error('Error fetching data:', error));
-  }
-  
-  function displayUsers(users) {
+        .then(response => response.json())
+        .then(data => displayUsers(data.results))
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+function displayUsers(users) {
     const circles = document.getElementsByClassName('circle');
     users.forEach((user, index) => {
-      if (circles[index]) {
-        appendUserToCircle(circles[index], user);
-      }
+        if (circles[index]) {
+            appendUserToCircle(circles[index], user);
+        }
     });
-  }
-  
-  function appendUserToCircle(circle, user) {
+}
+
+function appendUserToCircle(circle, user) {
     const img = createUserImage(user.picture.large);
     const name = createUserName(`${user.name.first} ${user.name.last}`);
     circle.append(img, name);
-  }
-  
-  function createUserImage(src) {
+}
+
+function createUserImage(src) {
     const img = document.createElement('img');
     img.src = src;
     return img;
-  }
-  
-  function createUserName(fullName) {
+}
+
+function createUserName(fullName) {
     const name = document.createElement('p');
     name.textContent = fullName;
     return name;
-  }
+}
