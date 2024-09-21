2a388d042cda2155b993dc100e935a25271fe588
DenyssePalomino03 <ivollmond03@gmail.com>
2024-09-21 00:29:44 -0600

    Update calificaciones.js

--- tasks/denyssepalominorojas/clase10/Ejercicios/7/calificaciones.js
+++ tasks/denyssepalominorojas/clase10/Ejercicios/7/calificaciones.js
@@ -9,11 +9,22 @@
     El script imprimirá el nombre y la media de sus calificaciones.
 */
 
-class Curso{
-    constructor(){
-        this.nombreA = nombre;
-        this.calificaciones = [];
-    }
+const alumno = {
+        //parseFloat es una función en js que se utiliza para convertir un string en un numero flotante (decimal)
+        nombre: prompt("Ingrese el nombre deel alumno:"),
+        ingles: parseFloat(prompt("Ingrese la calificación de Inglés:")),
+        programacion: parseFloat(prompt("Ingrese la calificación de Programación:")),
+        html: parseFloat(prompt("Ingrese la calificación de html:"),
 
-    
-}
+        // Método para calcular la media de las calificaciones
+        media: fuction() {
+                const total = this.ingles + this.programacion + this.html;
+                return total / 3;
+        }
+};
+
+// Calcular la media y mmostrar el resultado 
+const mediaCalificaciones = alumno.media();
+// alert sirve para mostrar un cuadro de dialogo simple al usuario
+// y aqui la utilicé para mostrar la media de calificaciones, e indicando que quiero dos decimales.
+alert(`Alumno: ${alumno.nombre}\n Media de calificaciones: ${mediaCalificaciones.toFixed(2)}`);
