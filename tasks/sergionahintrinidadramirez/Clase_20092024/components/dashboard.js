ce973811eda6d707b4407634071f7a892e8ffc21
Sergio Trinidad <sergio.trinidad@ids.com.mx>
2024-09-21 22:05:05 -0600

    feat: Implemented login functionality with event communication from bbva-app to bbva-login and bbva-dashboard

--- tasks/sergionahintrinidadramirez/Clase_20092024/components/dashboard.js
+++ tasks/sergionahintrinidadramirez/Clase_20092024/components/dashboard.js
@@ -2,30 +2,41 @@ import { html, LitElement } from "lit";
 import { styleDashboard } from "./styles/styleDashboard";
 
 export class Dashboard extends LitElement {
+    static properties = {
+        banco: {type: Object}
+    };
+
     static styles = styleDashboard
+    
     constructor() {
         super();
+        this.banco = {};
     }
 
     render() {
+        console.log(`Banco en Dashboard: ${JSON.stringify(this.banco)}`);
+        if(!this.banco){
+            return html `
+            <p>Cargando Información....</p>`
+        }
         return html`
         <h1 id="title">Dashboard</h1>
-            <section class="dashboard-container">
-                <h1>Hola, <span id="nombre-usuario"></span></h1>
-                <div class="cuenta">
-                    <h2>Cuenta en pesos</h2>
-                    <div class="info-cuenta">
-                        <span id="cuenta">Número de cuenta: xxxx-xxxx</span>
-                        <span id="saldo">Saldo: $0.00</span>
-                    </div>
-                </div>
-                <div class="tarjeta">
-                    <h2>Tarjeta</h2>
-                    <div class="info-tarjeta">
-                        <span id="tarjeta">xxxx-xxxx-xxxx-xxxx</span>
-                        <button id="detalles-tarjeta">Detalles</button>
-                    </div>
-                </div>
-            </section>`
+      <section class="dashboard-container">
+        <h1>Hola, <span id="nombre-usuario">${this.banco.usuario.nombre || 'Usuario'}</span></h1>
+        <div class="cuenta">
+          <h2>Cuenta en pesos</h2>
+          <div class="info-cuenta">
+            <span id="cuenta">Número de cuenta: ${this.banco.usuario.cuenta || 'xxxx-xxxx'}</span>
+            <span id="saldo">Saldo: $${this.banco.usuario.saldo || '0.00'}</span>
+          </div>
+        </div>
+        <div class="tarjeta">
+          <h2>Tarjeta</h2>
+          <div class="info-tarjeta">
+            <span id="tarjeta">${this.banco.usuario.tarjeta || 'xxxx-xxxx-xxxx-xxxx'}</span>
+            <button id="detalles-tarjeta">Detalles</button>
+          </div>
+        </div>
+      </section>`
     }
 }
